e22f9fd56b18682a4cd5c378a7057fc75037c542
anubhabb66@gmail.com <anubhabb66@gmail.com>
2023-08-16 16:21:42 +0530

    initial commit

---
+++ CourseApp/Models/Course.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CourseApp.Models
+{
+    [Table("Course")]
+    public class Course
+    {
+        [Key]
+        public int CId { get; set; }
+
+        [Required]
+        public string CName { get; set; }
+        [Required]
+        public decimal CFee { get; set; }
+        public string Status { get; set; }
+        public string Technology { get; set; }
+    }
+}
---
+++ CourseApp/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using CourseApp.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<CourseDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CourseDbContext") ?? throw new InvalidOperationException("Connection string 'CourseDbContext' not found.")));
+
+// Add services to the container.
+//builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("CourseConStr")));
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
